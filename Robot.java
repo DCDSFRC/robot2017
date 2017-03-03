20e1b644a5e57b3addc9c5b013465f9802a57f78
MAIN laptop <DCDSFRC@gmail.com>
2017-03-02 22:26:06 -0500

    not base

--- Robot.java
+++ Robot.java
@@ -10,6 +10,7 @@ package org.usfirst.frc.team835.robot;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import java.util.Arrays;
 
@@ -21,17 +22,46 @@ import java.util.Arrays;
  * directory.
  */
 public class Robot extends IterativeRobot {
-
+	final int[] buttonConstantsR = {//better if you make this a dictionary and use string names instead of indexes for when you reference the numbers
+	//	values	indexes		description
+		0,// 	- 			trigger for opening door
+		0,//	1	
+		2,//	2			slider shooter
+		0,//	3
+		0,//	4
+		0,//	5
+		0,//	6
+		7,//	7			reverse direction
+		8,//	8			reverse shooter
+		9,//	9			activate movement with trigger
+		10,//	10			high shooter
+		0,//	11	
+		12,//	12			low shooter
+		
+		
+};
+	final int[] buttonConstantsL = {
+		//slider for collector
+		
+		
+};
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
+	String autoSelected;
+	SendableChooser<String> chooser = new SendableChooser<>();
+	
+
+	final String defaultAuto = "Default";
+	final String customAuto = "My Auto";
+	final String choicesAuto = "Auto choices";
 	// Robot Drive Variable
 	private RobotDrive myRobot;
 	// Motor Controllers
-	private TalonSRX rightB, leftB, rightF, leftF, winch, collector, shooter;
+	private TalonSRX rightB, leftB, rightF, leftF, collector, shooter;
 	// Servo Motor
-	private Servo servo;
+//	private Servo servo;
 	// Camera Resolution
 	private int xRes = 240;
 	private int yRes = 360;
@@ -42,7 +72,8 @@ public class Robot extends IterativeRobot {
 	// Ultrasonic Sensor
 	Ultrasonic uss;
 	// Pneumatics
-	DoubleSolenoid ds;
+	DoubleSolenoid shooterClose;
+	DoubleSolenoid agitator;
 	Compressor compressor;
 	// Joysticks
 	private Joystick whiteR, whiteL;
@@ -51,8 +82,7 @@ public class Robot extends IterativeRobot {
 	private double SHOOTER_SPEED, BELT_SPEED, GYRO_ANGLE;
 	private double X, Y, Z;
 	private boolean isReversed;
-	double x, distance, power, curve = 0;
-	double[] xvalues, areas, heights, widths;
+	
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -62,7 +92,11 @@ public class Robot extends IterativeRobot {
 		table = NetworkTable.getTable("GRIP/targets");
 		NetworkTable.setTeam(835);
 		
-		
+		chooser.addDefault("Default Auto", defaultAuto);
+		chooser.addObject("My Auto", customAuto);
+		SmartDashboard.putData(choicesAuto, chooser);
+		autoSelected = chooser.getSelected();
+
 		// Instantiate Motor Controllers
 		rightB = new TalonSRX(0);
 		leftB = new TalonSRX(1);
@@ -87,7 +121,8 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putNumber("Original Gyro Angle", ORIGIN_ANGLE);
 
 		// Instantiate Pnuematics Components
-		ds = new DoubleSolenoid(0, 1);
+		shooterClose = new DoubleSolenoid(0, 1);
+		shooterClose = new DoubleSolenoid(2, 3);
 		compressor = new Compressor(0);
 		compressor.setClosedLoopControl(true);
 
@@ -112,62 +147,32 @@ public class Robot extends IterativeRobot {
 	 */
 	
 	public void autonomousInit() {
-		gyro.calibrate();
-		ORIGIN_ANGLE = gyro.getAngle();
-		SmartDashboard.putNumber("Original Gyro Angle", ORIGIN_ANGLE);
+
+		autoSelected = table.getString(choicesAuto, defaultAuto);
+		
 		gyro.reset();
 
-		// Starts with forward drive system (duh)
+		ORIGIN_ANGLE = gyro.getAngle();
+		SmartDashboard.putNumber("Original Gyro Angle", ORIGIN_ANGLE);
+
+		// Starts with drive system forwards, towards gear collector
 		isReversed = false;
 	}
 
 	public void autonomousPeriodic() {
-		updateDashboard("Ultrasonic", uss.getRangeInches());
-
 		
-//		setTables(table);
-		areas = table.getNumberArray("area", new double[0]);
-		if (areas.length == 0) {
-			myRobot.arcadeDrive(0, 0.7);
-			return;
-		}
-		heights = table.getNumberArray("height", new double[0]);
-		xvalues = table.getNumberArray("centerX", new double[0]);
-		if (xvalues.length == 0) {
-			x = xRes / 2;
-		} else {
-			x = xvalues[0];
-		}
-		curve = curveToCenter(x);
-		SmartDashboard.putNumber("X", x);
-		SmartDashboard.putNumber("PreCurve", curve);
-		curve *= -Math.sqrt(2.0);
-		if (curve > 0.5) {
-			curve = 0.5;
-		}
-		if (curve < -0.5) {
-			curve = -0.5;
-		}
-		if (heights.length != 0) {
-			distance = 5 / 12 * yRes / (2 * heights[0] * Math.tan(54 * Math.PI / 180));
-		} else {
-			distance = 0;
-		}
-
-		if (distance > 0.5) {
-			power = 5.0 / 9;
-		} else {
-			power = 0;
-		}
-		SmartDashboard.putNumber("PostCurve", curve);
-		SmartDashboard.putNumber("distance", distance);
-		SmartDashboard.putNumber("power", power);
+		double power=0, curve;
+		updateDashboard("Ultrasonic", uss.getRangeInches());
+		if(uss.getRangeInches() <= 12)
+			power = .3;
+		
 		// power = 0;
-		// curve = 0;
+		 curve = 0;
 		myRobot.arcadeDrive(power, curve);
+		System.out.println(""+ power+":"+ curve);
 	}
 	
-
+	
 	
 
 
@@ -179,9 +184,9 @@ public class Robot extends IterativeRobot {
 		// Always reset Gyro in init methods. You don't necessarily need to
 		// calibrate.
 		gyro.calibrate();
+		gyro.reset();
 		ORIGIN_ANGLE = gyro.getAngle();
 		SmartDashboard.putNumber("Original Gyro Angle", ORIGIN_ANGLE);
-		gyro.reset();
 
 		// Starts with forward drive system (duh)
 		isReversed = false;
@@ -194,8 +199,8 @@ public class Robot extends IterativeRobot {
 	public void teleopPeriodic() {
 		
 		updateDashboard("UltraSonic", uss.getRangeInches());
-		// Button 2 to reverse
-		if (whiteR.getRawButton(7)) {
+		// Button 7 to reverse
+		if (whiteR.getRawButton(buttonConstantsR[7])) {
 			isReversed = !isReversed;
 		}
 
@@ -211,16 +216,22 @@ public class Robot extends IterativeRobot {
 		runShooter();
 
 		// Use to get jammed balls out
-		if (whiteL.getRawButton(8)) {
-			shooter.set(-0.18);
-		}
+		
 
 		// Pneumatics Controls
 		if (whiteR.getTrigger()) {
-			ds.set(DoubleSolenoid.Value.kReverse);
+			shooterClose.set(DoubleSolenoid.Value.kReverse);
 			SmartDashboard.putString("Hopper Door: ", "OPEN");
 		} else {
-			ds.set(DoubleSolenoid.Value.kForward);
+			shooterClose.set(DoubleSolenoid.Value.kForward);
+			SmartDashboard.putString("Hopper Door: ", "CLOSED");
+		}
+		ButtonType
+		if (whiteR.getButton() {
+			shooterClose.set(DoubleSolenoid.Value.kReverse);
+			SmartDashboard.putString("Hopper Door: ", "OPEN");
+		} else {
+			shooterClose.set(DoubleSolenoid.Value.kForward);
 			SmartDashboard.putString("Hopper Door: ", "CLOSED");
 		}
 
@@ -267,15 +278,18 @@ public class Robot extends IterativeRobot {
 	 */
 	private void runShooter() {
 		SHOOTER_SPEED = 0;
-		//TODO:asdf
-		if (whiteR.getRawButton(12))
+		//TODO:change controls to something convenient
+		if (whiteR.getRawButton(buttonConstantsR[8])) {
+			shooter.set(-0.18);
+		}
+		else if (whiteR.getRawButton(buttonConstantsR[12]))
 		{
 			SHOOTER_SPEED = .25;
 		}
-		else if (whiteR.getRawButton(10)) {
+		else if (whiteR.getRawButton(buttonConstantsR[10])) {
 			SHOOTER_SPEED = .755;
 		}
-		else if (whiteR.getRawButton(2)) {
+		else if (whiteR.getRawButton(buttonConstantsR[2])) {
 			
 			SHOOTER_SPEED = roundDown(slider(whiteR), 0.1, 1.0);
 		}
@@ -313,8 +327,8 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putNumber("Gyro Angle: ", GYRO_ANGLE);
 		SmartDashboard.putNumber("Shooter Magnitude: ", SHOOTER_SPEED);
 		SmartDashboard.putNumber("Belt Speed: ", BELT_SPEED);
-//		SmartDashboard.putBoolean("Compressor Pressure Switch On: ", compressor.getPressureSwitchValue());
-//		SmartDashboard.putNumber("Compressor Current: ", compressor.getCompressorCurrent());
+		SmartDashboard.putBoolean("Compressor Pressure Switch On: ", compressor.getPressureSwitchValue());
+		SmartDashboard.putNumber("Compressor Current: ", compressor.getCompressorCurrent());
 	}
 	
 	/**
@@ -342,30 +356,123 @@ public class Robot extends IterativeRobot {
 		}
 	}
 
-	void goForward(double power) {
-		//Step 1: TODO: go forward past the airship
-		myRobot.mecanumDrive_Cartesian(0, power, 0, gyro.getAngle());
+	void steerForwardsGear() {
+		myRobot.mecanumDrive_Cartesian(0, getPowerGear(), getRotationGear(), gyro.getAngle());
 	}
+	
 
-	void steer() {
-		//Step 2: TODO: Rotate to a bearing facing the airship
-		if(GYRO_ANGLE < 200 || GYRO_ANGLE > 270){
-			myRobot.mecanumDrive_Cartesian(0, 0, .4, 0);
+	void leftLiftAutonomous() {
+		int step = 0;
+		long start = System.nanoTime();
+//		final double[] steps = {
+//				start,
+//				3 * Math.pow(10, 9),
+//				5 * Math.pow(10, 9),
+//				7 * Math.pow(10, 9),
+//				15 * Math.pow(10, 9),
+//				200
+//		}; 
+//			
+//		if(System.nanoTime() - steps[stepCount] < steps[stepCount +1]) {
+//			stepCount++;
+//		}
+		
+		switch(step){ 
+			case 0:
+				myRobot.mecanumDrive_Polar(.7, 0, 0);//TODO: find time
+				if(System.nanoTime() - start >= 3 * Math.pow(10, 9)) return;
+				step++;
+				//DRIVE forwards
+				break;
+			case 1:
+				if(getRotationGear() <= 20 && getRotationGear() >= -20) {
+					step++;
+				}
+				else myRobot.mecanumDrive_Polar(0, 0, .5);//turn and locate
+				break;
+			case 2:
+				if(getDistanceGearInches() != 0)
+					steerForwardsGear();
+				else step++;
+				//fowards and look for tape
+				break;
+//			case 3:
+//				if(getDistanceGearInches() <= 10)
+//					myRobot.mecanumDrive_Cartesian(0, -.5, getRotationGear(), gyro.getAngle());
+//				else step++;
+//				//backwards 
+//				break;
+			case 4:
+				
+				break;
+			case 5:
+				
+				break;
+				//go for ending
+			default:
+				myRobot.mecanumDrive_Polar(0, 0, 0);
+				return;
 		}
+		
 	}
-
-	void steerForward() {
-		//Step 3: TODO: Use UltraSonic to get distance to the airship
-		int xPos = 0;
-		myRobot.mecanumDrive_Cartesian(0, .35, curveToCenter(xPos), GYRO_ANGLE);
+	double getDistanceGearInches() {
+		double distance = uss.getRangeInches();
+//		double[] heights;
+//		double distance;
+//		heights = table.getNumberArray("height", new double[0]);
+//
+//		if (heights.length != 0) {
+//			distance = 5 / 12 * yRes / (2 * heights[0] * Math.tan(54 * Math.PI / 180));
+//		} else {
+//			distance = 0;
+//		}
+		return distance;
 	}
+	double getPowerGear() {
+		double distance, power;
+		distance = getDistanceGearInches();
 
-	double curveToCenter(double pos) {
-		if (Math.abs(pos - xRes / 2) > 20) {
-			return (pos - xRes / 2) / xRes;
+		if (distance > 0.5) {
+			power = 5.0 / 9;
 		} else {
-			return 0.0;
+			power = 0;
 		}
+		SmartDashboard.putNumber("distance", distance);
+
+		SmartDashboard.putNumber("power", power);
+		return power;
+	}
+	double getRotationGear() {
+		double x, curve = 0;
+		double[] xvalues, areas;
+		areas = table.getNumberArray("area", new double[0]);
+		if (areas.length == 0) {
+			return .7;
+		}
+		xvalues = table.getNumberArray("centerY", new double[0]);//accounts for camera rotation
+		if (xvalues.length < 2) {
+			x = xRes / 2;
+		} else {
+			x = xvalues[0] + xvalues[1];
+			x /= 2;
+		}
+		curve = cameraXtoRotation(x);
+		SmartDashboard.putNumber("CenterX", x);
+		SmartDashboard.putNumber("PostCurve", curve);
+		return curve;
+	}
+	double cameraXtoRotation(double x) {
+		return -(x - xRes / 2) / xRes;
+	}
+	double roundToHalf(double value) {
+		value *= Math.sqrt(2.0);
+		if (value > 0.5) {
+			return 0.5;
+		}
+		if (value < -0.5) {
+			return -0.5;
+		}
+		else return 0;
 	}
 
 	void gyroscope() {
